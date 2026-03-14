13704266b5364d5a09c057873343094b38c466ed
vatsakshat02 <vatsakshat02@gmail.com>
2026-03-15 02:01:41 +0530

    chore: Fixed screen width

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -14,8 +14,7 @@ const Videobackground = ({ movieId }: VideoBackgroundprops) => {
   return (
     <div>
       <iframe
-        width="560"
-        height="315"
+        className="w-screen aspect-video"
         src={"https://www.youtube.com/embed/" + trailerVideo?.key}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
--- src/components/VideoTitle.tsx
+++ src/components/VideoTitle.tsx
@@ -7,9 +7,9 @@ interface VideoTitleProps {
 
 const VideoTitle = ({ title, overview }: VideoTitleProps) => {
   return (
-    <div className="pt-36 px-12">
-      <h1 className="font-bold text-6xl">{title}</h1>
-      <p className="py-6 text-lg w-1/4">{overview}</p>
+    <div className="pt-36 px-12 absolute my-40 bg-gradient-to-r from-black">
+      <h1 className="font-bold text-6xl text-white">{title}</h1>
+      <p className="py-6 text-lg w-1/4 text-white">{overview}</p>
       <div>
         <button className="bg-gray-500 text-white p-4 px-14 text-xl bg-opacity-50 rounded-lg mr-2">
           ▶️ Play
